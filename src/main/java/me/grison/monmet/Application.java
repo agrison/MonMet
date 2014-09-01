a6a8f50a9ef189bb0a8ab41f15a22ab326ffdae1
Alexandre Grison <a.grison@gmail.com>
2014-09-01 08:40:45 +0200

    imports

--- src/main/java/me/grison/monmet/Application.java
+++ src/main/java/me/grison/monmet/Application.java
@@ -1,19 +1,12 @@
 package me.grison.monmet;
 
-import com.google.gson.GsonBuilder;
-import me.grison.monmet.timetable.StopsService;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-
-import java.io.File;
-import java.util.Arrays;
 
 /**
  * MonMet' application.
